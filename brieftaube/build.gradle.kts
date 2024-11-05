7335e9fc5070cb9f69f712a8403b26940db0e0a8
Michael Rittmeister <michael@rittmeister.in>
2024-11-05 21:39:37 +0100

    Fix invalid translation of brieftaube file

--- brieftaube/build.gradle.kts
+++ brieftaube/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     alias(libs.plugins.kotlin.serialization)
 }
 
-version = "2.0.1"
+version = "2.0.2"
 
 mikbotPlugin {
     description = "Plugin allowing you to mirror channels in threads"
